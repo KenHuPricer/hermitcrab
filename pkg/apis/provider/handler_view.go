4fb337802c47f7508135c5d539d56c177fa28490
KenHuPricer <146701727+KenHuPricer@users.noreply.github.com>
2025-08-20 10:12:53 +0800

    feat(provider): support provider names that contain a hyphen

--- pkg/apis/provider/handler_view.go
+++ pkg/apis/provider/handler_view.go
@@ -71,7 +71,7 @@ func (r *DownloadArchiveRequest) SetGinContext(ctx *gin.Context) {
 }
 
 var regexValidArchive = regexp.MustCompile(
-	`^terraform-provider-(?P<type>\w+)[_-](?P<version>[\w|\\.]+)[_-](?P<os>[a-z]+)[_-](?P<arch>[a-z0-9]+)([_-].*)?\.zip$`,
+	`^terraform-provider-(?P<type>[\w-]+)[_-](?P<version>[\w|\\.]+)[_-](?P<os>[a-z]+)[_-](?P<arch>[a-z0-9]+)([_-].*)?\.zip$`,
 )
 
 func (r *DownloadArchiveRequest) Validate() error {
